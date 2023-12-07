7264d8cfb1d327baa3ec034d598ff667082828fb
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2023-12-07 16:56:54 +0000

    update readme and provide non lfs route

--- setup.py
+++ setup.py
@@ -13,18 +13,20 @@ class install(_install):
         import zipfile
         cwd = os.getcwd()
         gitdir = os.path.dirname(os.path.realpath(__file__))
-        os.chdir(gitdir)
-        g = git.cmd.Git(gitdir)
-        try:
-            g.execute(['git', 'lfs', 'install'])
-            g.execute(['git', 'lfs', 'fetch'])
-            g.execute(['git', 'lfs', 'pull'])
-            zip_data = os.path.join(gitdir, "virbot", "data", "ref.zip")
-            with zipfile.ZipFile(zip_data,"r") as zip_ref:
-                zip_ref.extractall()
-        except git.exc.GitCommandError:
-            print("Warning git-lfs is not installed - please manually download and unzip the reference files!")
-        os.chdir(cwd)
+        data_dir = os.path.join(gitdir, "virbot", "data", "ref")
+        if not os.listdir(data_dir):
+            os.chdir(gitdir)
+            g = git.cmd.Git(gitdir)
+            try:
+                g.execute(['git', 'lfs', 'install'])
+                g.execute(['git', 'lfs', 'fetch'])
+                g.execute(['git', 'lfs', 'pull'])
+                zip_data = os.path.join(gitdir, "virbot", "data", "ref.zip")
+                with zipfile.ZipFile(zip_data,"r") as zip_ref:
+                    zip_ref.extractall()
+            except git.exc.GitCommandError:
+                print("Warning git-lfs is not installed - please manually download and unzip the reference files!")
+            os.chdir(cwd)
 
     def run(self):
         self.pull_first()
@@ -34,7 +36,7 @@ setup(name='virbot',
       version=__version__,
       packages=find_packages(),
       package_data={"virbot":["data/ref/*"]},
-      install_requires=['git-lfs'],
+      install_requires=[],
       setup_requires=["git-python"],
       description='VirBot: RNA viral contig detector for metagenomic data',
       url='https://github.com/GreyGuoweiChen/RNA_virus_detector.git',
